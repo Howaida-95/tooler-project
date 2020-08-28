af5f0a0c336d7bb198c93cf3a9d6e71d8d520f2f
Howaida-S <61283789+Howaida-S@users.noreply.github.com>
2020-08-28 20:48:22 +0200

    Update _header.php

--- pages/_header.php
+++ pages/_header.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Example</title>
-    <link rel="stylesheet" href="<?php assets("./images/tooler.ico"); ?>">
     <link rel="stylesheet" href="<?php assets("./css/bootstrap.min.css"); ?>">
     <link rel="stylesheet" href="<?php assets("/css/home.css"); ?>">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
